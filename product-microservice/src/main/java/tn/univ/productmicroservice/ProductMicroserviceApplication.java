c0e11d7d935324fa1b18499d1ae9020f4fd86454
Fady Zaafrane <fady.zaafrane@gmail.com>
2024-11-12 17:34:48 +0100

    Add Product Microservice

---
+++ product-microservice/src/main/java/tn/univ/productmicroservice/ProductMicroserviceApplication.java
@@ -0,0 +1,7 @@
+package tn.univ.productmicroservice;
+
+public class ProductMicroserviceApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
